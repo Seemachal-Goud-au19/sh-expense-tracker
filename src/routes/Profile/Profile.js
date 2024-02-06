ed327163bdcf87ff8d9eee81f53e0363c6509c25
Seemachal-Goud-au19 <simanchal9566@gmail.com>
2024-02-06 21:28:44 +0530

    Edit User Details and show user details in fields on refresh

--- src/routes/Profile/Profile.js
+++ src/routes/Profile/Profile.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
 import { FaGithub } from "react-icons/fa";
@@ -8,14 +8,61 @@ import './Profile.css'
 
 
 const Profile = () => {
-    const inputName = useRef();
-    const inputProfileUrl = useRef();
+    const [userData, setUserData] = useState({
+        profile_name: '',
+        profile_photo: ''
+    })
+
     const navigate = useNavigate();
+
+    //
+
+    const getUser = () => {
+
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBI0CjaNxfs8gZIR5xd6R8wBKn14aZo2qs', {
+            method: 'POST',
+            body: JSON.stringify({
+                idToken: localStorage.getItem('token'),
+
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+
+            if (!response.ok) {
+                return
+            }
+            response.json().then((data) => {
+                setUserData({
+                    profile_name: data?.users[0].displayName,
+                    profile_photo: data?.users[0].photoUrl
+                })
+            })
+
+        }).catch((err) => {
+            console.log(err)
+        })
+
+    }
+    useEffect(() => {
+        getUser()
+    }, [])
+    //
+
+    const InputChangeHandler = (e, type) => {
+        setUserData((prevData) => {
+            return {
+                ...prevData,
+                [type]: e.target.value
+            }
+        })
+    }
 
     const profileSubmithandler = (e) => {
         e.preventDefault();
-        const enteredName = inputName.current.value;
-        const enteredProfileUrl = inputProfileUrl.current.value;
+        const enteredName = userData.profile_name;
+        const enteredProfileUrl = userData.profile_photo;
         fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBI0CjaNxfs8gZIR5xd6R8wBKn14aZo2qs', {
             method: 'POST',
             body: JSON.stringify({
@@ -47,15 +94,16 @@ const Profile = () => {
                 <div className='form-field-container'>
                     <div className=''>
                         <label htmlFor='profile-name'><FaGithub />Full Name</label>
-                        <input type='text' id='profile-name' required ref={inputName} />
+                        <input type='text' id='profile-name' value={userData.profile_name} onChange={(e) => InputChangeHandler(e, 'profile_name')} />
                     </div>
                     <div className=''>
                         <label htmlFor='profile-url'><TbWorld />Profile Photo URL</label>
                         <input
                             type="text"
                             id='profile-url'
+                            value={userData.profile_photo}
                             required
-                            ref={inputProfileUrl}
+                            onChange={(e) => InputChangeHandler(e, 'profile_photo')}
                         />
 
                     </div>
